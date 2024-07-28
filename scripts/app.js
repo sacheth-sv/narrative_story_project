bb5e5f50a0b77a3e7bbdd74c9fccf4ff7afa2ac6
Sacheth Chandramouli <c.sacheth1@gmail.com>
2024-07-28 14:27:42 -0500

    minor wording tweaks

--- scripts/app.js
+++ scripts/app.js
@@ -530,7 +530,7 @@ async function render_lineplot_slide_3() {
             var selected_state = d3.select("#stateSelectionDropdown").node().value;
             var state_data = data.filter(function(d) {return d.state == selected_state;});
             var metric = d3.select(this).node().value
-            if (metric == "cases") {
+            if (metric == "New Infections") {
                 update_lineplot_slide_3(state_data, "rolling_avg_cases");
             } else {
                 update_lineplot_slide_3(state_data, "rolling_avg_fatalities");
